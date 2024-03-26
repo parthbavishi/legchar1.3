d4148985ff8cfc9252499e0c42e10c8950b3d05e
parthbavishi <165029489+parthbavishi@users.noreply.github.com>
2024-03-26 18:04:58 +0530

    Add files via upload

---
+++ q.7.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main(){
+	
+	int a,b;
+	 
+	printf("enter your first number:\n ");
+	scanf("%d",&a);
+	printf("enter your second number:\n ");
+	scanf("%d",&b);
+	 	 
+		printf("%d*%d=%d",a,b,a*b);
+	
+	
+}
---
+++ q3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+	
+	int a=102;
+	int b=123;
+	
+	printf("%d*%d=%d",a,b,a*b);
+	
+	
+	
+	
+	
+}
